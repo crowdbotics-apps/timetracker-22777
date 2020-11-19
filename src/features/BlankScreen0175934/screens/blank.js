26271c2d2953a3779f7bac8d0573bc57e1b63bb3
Crowdbotics <team@crowdbotics.com>
2020-11-19 17:05:24 +0000

    Update components

--- src/features/BlankScreen0175934/screens/blank.js
+++ src/features/BlankScreen0175934/screens/blank.js
@@ -23,12 +23,26 @@ export default class Blank extends React.Component {
     }
   }
 
-  state = { TextInput_6: "" }
+  state = {
+    TextInput_6: "",
+    TextInput_75: "",
+    TextInput_79: "",
+    TextInput_85: "",
+    TextInput_89: "",
+    TextInput_91: "",
+    TextInput_95: "",
+    TextInput_108: "",
+    TextInput_100: "",
+    TextInput_118: "",
+    TextInput_121: ""
+  }
 
   render = () => (
     <View style={styles.View_1}>
       <TextInput
-        placeholder="Sample text input placeholder"
+        placeholder="Morning Plan"
+        editable={false}
+        style={styles.TextInput_6}
         value={this.state.TextInput_6}
         onChangeText={nextValue => this.setState({ TextInput_6: nextValue })}
       />
@@ -36,27 +50,112 @@ export default class Blank extends React.Component {
         <View>
           <View>
             <View />
-            <View />
+            <View>
+              <TextInput
+                placeholder="9"
+                style={styles.TextInput_75}
+                value={this.state.TextInput_75}
+                onChangeText={nextValue =>
+                  this.setState({ TextInput_75: nextValue })
+                }
+              />
+            </View>
           </View>
           <View>
-            <View />
-            <View />
+            <View>
+              <TextInput
+                placeholder="10"
+                style={styles.TextInput_79}
+                value={this.state.TextInput_79}
+                onChangeText={nextValue =>
+                  this.setState({ TextInput_79: nextValue })
+                }
+              />
+            </View>
+            <View>
+              <TextInput
+                placeholder="11"
+                style={styles.TextInput_85}
+                value={this.state.TextInput_85}
+                onChangeText={nextValue =>
+                  this.setState({ TextInput_85: nextValue })
+                }
+              />
+            </View>
           </View>
           <View>
-            <View />
+            <View>
+              <TextInput
+                placeholder="12"
+                style={styles.TextInput_89}
+                value={this.state.TextInput_89}
+                onChangeText={nextValue =>
+                  this.setState({ TextInput_89: nextValue })
+                }
+              />
+            </View>
           </View>
         </View>
         <View>
           <View>
-            <View />
-            <View />
+            <View>
+              <TextInput
+                placeholder=":00"
+                editable={false}
+                value={this.state.TextInput_91}
+                onChangeText={nextValue =>
+                  this.setState({ TextInput_91: nextValue })
+                }
+              />
+            </View>
+            <View>
+              <TextInput
+                placeholder="Sample text input placeholder"
+                value={this.state.TextInput_95}
+                onChangeText={nextValue =>
+                  this.setState({ TextInput_95: nextValue })
+                }
+              />
+            </View>
           </View>
           <View>
-            <View />
-            <View />
+            <View>
+              <TextInput
+                placeholder="Sample text input placeholder"
+                value={this.state.TextInput_108}
+                onChangeText={nextValue =>
+                  this.setState({ TextInput_108: nextValue })
+                }
+              />
+            </View>
+            <View>
+              <TextInput
+                placeholder="Sample text input placeholder"
+                value={this.state.TextInput_100}
+                onChangeText={nextValue =>
+                  this.setState({ TextInput_100: nextValue })
+                }
+              />
+            </View>
           </View>
           <View>
-            <View />
+            <View>
+              <TextInput
+                placeholder="Sample text input placeholder"
+                value={this.state.TextInput_118}
+                onChangeText={nextValue =>
+                  this.setState({ TextInput_118: nextValue })
+                }
+              />
+              <TextInput
+                placeholder=":15"
+                editable={false}
+                value={this.state.TextInput_121}
+                onChangeText={nextValue =>
+                  this.setState({ TextInput_121: nextValue })
+                }
+              />
+            </View>
           </View>
         </View>
         <View>
@@ -108,6 +207,7 @@ const styles = StyleSheet.create({
     flex: 1,
     marginHorizontal: 16
   },
+
   View_1: { alignContent: "space-between" },
   TextInput_6: {},
   View_9: { flexDirection: "row" },
@@ -129,6 +229,64 @@ const styles = StyleSheet.create({
   View_31: {},
   View_32: {},
   View_33: {},
+  View_12: {},
+  View_37: {},
+  View_38: {},
+  View_39: {},
+  View_40: {},
+  View_41: {},
+  View_42: {},
+  View_43: {},
+  View_44: {},
+  View_13: {},
+  View_34: {},
+  View_35: {},
+  View_36: {},
+  View_46: {},
+  View_47: {},
+  View_48: {},
+  View_49: {},
+  View_50: {},
+  View_14: {},
+  View_52: {},
+  View_53: {},
+  View_54: {},
+  View_55: {},
+  View_56: {},
+  View_57: {},
+  View_58: {},
+  View_59: {},
+  View_1: { alignContent: "space-between" },
+  TextInput_6: { fontFamily: "Montserrat-Regular", textAlign: "center" },
+  View_9: { width: 0, flexDirection: "row" },
+  View_10: {},
+  View_15: {},
+  View_16: {},
+  View_17: {},
+  TextInput_75: { width: "018%" },
+  View_18: {},
+  View_19: {},
+  TextInput_79: { width: "18%" },
+  View_20: {},
+  TextInput_85: { width: "18%" },
+  View_21: {},
+  View_22: {},
+  TextInput_89: { width: "18%" },
+  View_11: {},
+  View_23: {},
+  View_24: {},
+  TextInput_91: {},
+  View_25: {},
+  TextInput_95: {},
+  View_29: {},
+  View_30: {},
+  TextInput_108: {},
+  View_31: {},
+  TextInput_100: {},
+  View_32: {},
+  View_33: {},
+  TextInput_118: {},
+  TextInput_121: {},
   View_12: {},
   View_37: {},
   View_38: {},
